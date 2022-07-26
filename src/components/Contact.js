8756587967fe3a4abf95d14d4b8db52a34e6b96f
Mkdenno <dennis.kiprop@student.moringaschool.com>
2022-07-26 11:25:32 +0300

    it will add icons to contact us page

---
+++ src/components/Contact.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import {FaEnvelope,FaTwitter,FaLinkedin} from 'react-icons/fa'
+import styled from 'styled-components'
+// import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+
+function Contact() {
+  return (
+    <div>
+      <Header>Contact Us</Header>
+      <ContactUs>
+      <Icons>
+        <h1>Contact Information</h1>
+      <h3>Nairobi, Kenya</h3>
+      <label><i><FaEnvelope/></i>Email</label><br/>
+      <label><i><FaTwitter/></i>Twitter</label><br/>
+      <label><i><FaLinkedin/></i>LinkedIn</label><br/>
+      </Icons>
+
+      </ContactUs>
+      
+    </div>
+  )
+}
+
+export default Contact
