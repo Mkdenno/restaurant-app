7772fb2af97da4b5f746f7877e4a99c41d66fe63
Mkdenno <dennis.kiprop@student.moringaschool.com>
2022-07-26 11:23:48 +0300

    It will add about us page

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div className='aboutpage'>
+      <div className='aboutimage'>
+      <img src='./image/dennis.jpeg' alt='image'/>
+      </div>
+
+    </div>
+  )
+}
+
+export default About
