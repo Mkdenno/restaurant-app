e4c269dac9f5452fab5a044f882e12dee0c67ffb
Mkdenno <dennis.kiprop@student.moringaschool.com>
2022-07-26 11:24:31 +0300

    It will add more information in about us page

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,15 @@ function About() {
       <div className='aboutimage'>
       <img src='./image/dennis.jpeg' alt='image'/>
       </div>
+      <div className='aboutMore'>
+        <h1>About our Restaurant</h1>
+        <h2>We provide Good quality food to our customers!!</h2>
+        <p>We provide Good quality food to our customers We provide
+           Good quality food to our customers We provide Good quality food to
+            our customers We provide Good quality food to our customers</p>
 
+            <button className='aboutbutton'>Learn More</button>
+      </div>
     </div>
   )
 }
